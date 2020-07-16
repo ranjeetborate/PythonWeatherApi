065fce24d627019572e56404fe50d5d318a85572
ranjeet.borate <ranjeet.borate@gmail.com>
2020-07-16 10:28:31 +0530

    Results rendered

--- weatherApi/weatherApi.py
+++ weatherApi/weatherApi.py
@@ -12,13 +12,22 @@ def weather_dashboard():
 
 @app.route('/results', methods=['POST'])
 def results():
- zip_code = request.form['zipCode']    
- return "Zip Code: " + zip_code
+ # ~ zip_code = request.form['zipCode']    
+ # ~ return "Zip Code: " + zip_code
+ zip_code = request.form['zipCode']
+ api_key = get_api_key()
+ data = get_weather_results(zip_code, api_key)
+ print(data)
+ temp = "{0:.2f}".format(data["main"]["temp"])
+ feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+ weather = data["weather"][0]["main"]
+ location = data["name"]
+ return render_template('results.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
 
 def get_api_key():
  config = configparser.ConfigParser()
  config.read('config.ini')
- return config['openweathermap']['api']
+ return config['WeatherMapApiDetails']['api']
 
 def get_weather_results(zip_code, api_key):
  api_url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
